47cce857111dc7baf82ef63f300c53e66548ea37
aaron61083511 <aaron_61083511@126.com>
2019-03-03 22:41:53 -0600

    Add files via upload

---
+++ findemails.py
@@ -0,0 +1,29 @@
+import re
+import requests
+import pandas as pd
+import time
+
+data = pd.read_csv('/Users/aaron/Desktop/Rice University/FEP/custodial and ground/February 25/GROUNDS Survey Data 01312019.csv')
+target = data['REQOR_FIRST_NAME'] + '+' + data['REQOR_LAST_NAME']
+
+email_list = []
+regex = r"([a-zA-Z0-9_.+-]+@rice.edu)"
+counter = 0
+
+for i in range(len(target)):
+    if divmod(i, 10)[1] == 0:
+        time.sleep(60)
+        url = "https://search.rice.edu/?q=" + target[i]
+        plain_text = requests.get(url).text
+        email = re.findall(regex, plain_text)
+        email_list.append(email)
+        print(email)
+    else:
+        url = "https://search.rice.edu/?q=" + target[i]
+        plain_text = requests.get(url).text
+        email = re.findall(regex, plain_text)
+        email_list.append(email)
+        print(email)
+
+
+print(email_list)
